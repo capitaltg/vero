ed48d2b214a8eab6262fca34f43d059020e33aed
David.Z.Williams <dwilliams@capitaltg.com>
2025-06-04 17:26:39 -0400

    chore: use default primary colors instead of '-400' colors

--- src/components/Calendar/src/Calendar.tsx
+++ src/components/Calendar/src/Calendar.tsx
@@ -45,7 +45,7 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         day_range_start: 'day-range-start',
         day_range_end: 'day-range-end',
         day_selected:
-          'bg-primary-400 text-primary-foreground hover:bg-primary-400 hover:text-primary-foreground focus:bg-primary-400 focus:text-primary-foreground',
+          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
         day_today: 'bg-accent text-accent-foreground',
         day_outside: 'day-outside text-muted-foreground opacity-50',
         day_disabled: 'text-muted-foreground opacity-50',

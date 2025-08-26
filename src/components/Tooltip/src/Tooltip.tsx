c1984fc6aef2ee16ea8084d8a134f2ed8bed2074
David.Z.Williams <dwilliams@capitaltg.com>
2025-08-26 01:17:09 -0400

    feat: resolve Tooltip z-index from props or global config

--- src/components/Tooltip/src/Tooltip.tsx
+++ src/components/Tooltip/src/Tooltip.tsx
@@ -1,5 +1,6 @@
 import { styles } from '@/lib/styles';
 import { cn } from '@/lib/utils';
+import { getZIndex } from '@/lib/z-index';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import * as React from 'react';
 import { TooltipProps } from '../types';
@@ -16,29 +17,37 @@ const Tooltip = React.forwardRef<React.ElementRef<typeof TooltipPrimitive.Root>,
       offset = 2,
       hasArrow = true,
       className,
-      zIndex = 50,
+      zIndex,
       ...props
     },
     ref,
-  ) => (
-    <TooltipPrimitive.Root>
-      <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
-      <TooltipPrimitive.Content
-        ref={ref}
-        side={side}
-        align={align}
-        sideOffset={offset}
-        className={cn(styles.tooltip, className)}
-        style={{ zIndex, ...props.style }}
-        {...props}
-      >
-        {content}
-        {hasArrow ? (
-          <TooltipPrimitive.Arrow width={11} height={5} className="fill-base-700 stroke-base-700" />
-        ) : null}
-      </TooltipPrimitive.Content>
-    </TooltipPrimitive.Root>
-  ),
+  ) => {
+    const resolvedZIndex = getZIndex('tooltip', zIndex);
+
+    return (
+      <TooltipPrimitive.Root>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Content
+          ref={ref}
+          side={side}
+          align={align}
+          sideOffset={offset}
+          className={cn(styles.tooltip, className)}
+          style={{ zIndex: resolvedZIndex, ...props.style }}
+          {...props}
+        >
+          {content}
+          {hasArrow ? (
+            <TooltipPrimitive.Arrow
+              width={11}
+              height={5}
+              className="fill-base-700 stroke-base-700"
+            />
+          ) : null}
+        </TooltipPrimitive.Content>
+      </TooltipPrimitive.Root>
+    );
+  },
 );
 Tooltip.displayName = 'Tooltip';
 

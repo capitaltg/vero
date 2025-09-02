a0a87ff2dce48b02e6c1389f27bcc4f69c9cce8e
David.Z.Williams <dwilliams@capitaltg.com>
2025-09-02 00:11:27 -0400

    feat: use aria-disabled and new 'useDisabled' hook for handling disabled state in Button component

--- src/components/Button/src/Button.tsx
+++ src/components/Button/src/Button.tsx
@@ -1,3 +1,4 @@
+import { useDisabled } from '@/hooks';
 import { cn } from '@/lib/utils';
 import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
@@ -7,11 +8,13 @@ import { ButtonProps } from '../types';
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, isDisabled = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
+    const disabledProps = useDisabled({ isDisabled });
+
     return (
       <Comp
         ref={ref}
-        disabled={isDisabled}
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size }), className)}
+        {...disabledProps}
         {...props}
       />
     );

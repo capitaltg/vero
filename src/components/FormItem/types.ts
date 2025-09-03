ab380cbba4cce7c01547e40a00960479dd93cc3a
David.Z.Williams <dwilliams@capitaltg.com>
2025-09-03 15:45:23 -0400

    chore: added comments to FormItem types

--- src/components/FormItem/types.ts
+++ src/components/FormItem/types.ts
@@ -1,12 +1,36 @@
 import { ReactElement, ReactNode } from 'react';
 
 export interface FormItemProps {
+  /**
+   * The label for the form item. If a string is provided, it will be wrapped in a `<Label>` component. If a ReactNode is provided, it will be rendered as-is.
+   */
   label?: ReactNode;
+  /**
+   * An optional slot to display next to the label, such as an info icon with a tooltip or popover.
+   */
   labelSlot?: ReactNode;
+  /**
+   * Hint text to display below the label and above the form element.
+   */
   hintText?: string;
+  /**
+   * Error text to display below the form element. If provided, the form item will be styled to indicate an error.
+   */
   errorText?: string;
+  /**
+   * Additional classes to apply to the form item container.
+   */
   className?: string;
-  children: ReactElement;
+  /**
+   * The ID of the form element associated with this form item. This ID will be also be used to generate the label, hint, and error IDs for accessibility. If not provided, the ID of the child element will be used if available.
+   */
   elementId?: string;
+  /**
+   * The form element associated with this form item, such as an input or select.
+   */
+  children: ReactElement;
+  /**
+   * Whether the form item is required. If true, an asterisk (*) will be displayed next to the label.
+   */
   isRequired?: boolean;
 }

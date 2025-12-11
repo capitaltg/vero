0c162d3791d4ee2544cdf398bad4bc1ee01ada74
David.Z.Williams <dwilliams@capitaltg.com>
2025-12-10 21:36:45 -0500

    feat: default to manual activation for Tabs

--- src/components/Tabs/src/Tabs.tsx
+++ src/components/Tabs/src/Tabs.tsx
@@ -5,9 +5,7 @@ import * as React from 'react';
 import { TabsContentProps, TabsListProps, TabsProps, TabsTriggerProps } from '../types';
 
 const Tabs = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Root>, TabsProps>(
-  ({ activationMode = 'manual', ...props }, ref) => (
-    <TabsPrimitive.Root ref={ref} activationMode={activationMode} {...props} />
-  ),
+  ({ ...props }, ref) => <TabsPrimitive.Root ref={ref} activationMode="manual" {...props} />,
 );
 Tabs.displayName = TabsPrimitive.Root.displayName;
 
--- src/components/Tabs/types.ts
+++ src/components/Tabs/types.ts
@@ -10,10 +10,6 @@ export interface TabsProps extends ComponentPropsWithoutRef<typeof TabsPrimitive
    * The default value of the active tab when the component is uncontrolled.
    */
   defaultValue?: string;
-  /**
-   * How tabs are activated. When set to "automatic", tabs are activated when receiving focus. When set to "manual", tabs are activated when clicked.
-   */
-  activationMode?: 'automatic' | 'manual';
 }
 
 export type TabsListProps = ComponentPropsWithoutRef<typeof TabsPrimitive.List>;
